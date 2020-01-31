304ab8173b888a6162eaa7d00bc570c8f86a706d
Mahmmuod <mahmmuod.b2010@gmail.com>
2020-02-01 00:07:34 +0200

    Add mongoose connection initialization

--- server.js
+++ server.js
@@ -4,10 +4,23 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT
 require('dotenv').config();
 
+const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD);
+
 //INITIALIZE EXPRESS
 const app=express();
 //MIDDLEWARERS
 app.use(bodyParser.json());
+
+//MONGOOSE CONNECTION
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('DB connection successfully!');
+});
+
 
 //ROUTES
 app.get("/", (req, res) => {
